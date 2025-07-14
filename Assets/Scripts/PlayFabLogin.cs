6d90cfb2217cde9e7d6310075d05f82c2df83025
Alan <alan.hanafi@gemelligames.com>
2025-07-14 23:08:13 +0200

    Implement playfab custom login

---
+++ Assets/Scripts/PlayFabLogin.cs
@@ -0,0 +1,40 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+    private string customId;
+
+    public void UpdateCustomId(string newCustomId)
+    {
+        customId = newCustomId;
+    }
+    
+    public void LoginWithCustomID()
+    {
+        var request = new LoginWithCustomIDRequest { CustomId = customId, CreateAccount = true};
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+    }
+    
+    public void LoginWithSteam()
+    {
+        var request = new LoginWithSteamRequest();
+        PlayFabClientAPI.LoginWithSteam(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you logged in!");
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your login.");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+    
+    
+}
